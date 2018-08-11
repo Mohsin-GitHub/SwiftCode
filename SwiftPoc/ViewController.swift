ddcbc042494e106e293db79123ce1e6dca6c5f07
Nemat Bano <nematengg@gmail.com>
2018-08-12 05:48:50 +0800

    Initial Commit

---
+++ SwiftPoc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPoc
+//
+//  Created by Mohsin Ahmed on 12/8/18.
+//  Copyright © 2018 Mohsin Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
